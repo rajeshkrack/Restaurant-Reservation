a0e184e2a2db541a62bbe29ae8927bf5669810ca
rajeshkrack <rajeshkumbhakarn979@gmail.com>
2024-07-09 14:22:28 +0530

    Handled CORS Policy for backend url

--- frontend/src/components/Reservation.jsx
+++ frontend/src/components/Reservation.jsx
@@ -18,7 +18,7 @@ const Reservation = () => {
     e.preventDefault();
     try {
       const { data } = await axios.post(
-        "http://localhost:5000/api/v1/reservation/send",
+        "https://restaurant-reservation-backend-f8tp.onrender.com/api/v1/reservation/send",
         { firstName, lastName, email, phone, date, time },
         {
           headers: {
